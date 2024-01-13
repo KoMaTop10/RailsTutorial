19273ce7c70c61e2cd4cf523a8693896af89264d
KoMaTop10 <y.komachi10@gmail.com>
2024-01-13 20:37:55 +0000

    next 8.2.3

---
+++ sample_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      flash.now[:success] = "Hello #{user.name}!!"
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "static_pages#home"
   get "/home",     to: "static_pages#home"
   get  "/help",    to: "static_pages#help"
@@ -10,6 +7,9 @@ Rails.application.routes.draw do
   get  "/contact", to: "static_pages#contact"
 
   get 'signup', to: 'users#new', as: :signup
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
