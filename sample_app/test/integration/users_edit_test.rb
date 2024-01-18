845b5e51ce032b004bffb487bee45f5ab8c3aa3c
KoMaTop10 <y.komachi10@gmail.com>
2024-01-18 15:59:19 +0000

    認証機能の追加

--- sample_app/test/integration/users_edit_test.rb
+++ sample_app/test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'unsuccessful edit' do
+    log_in_as @user
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: {
@@ -20,6 +21,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'successful edit' do 
+    log_in_as @user
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
@@ -38,4 +40,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email,@user.email
   end
+
+  test 'successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    assert_redirected_to login_url
+    log_in_as @user
+    assert_redirected_to edit_user_path(@user)
+
+    name = 'Komachi'
+    email = 'komachi@komachi.com'
+    patch user_path(@user), params: { user: {
+      name: name,
+      email: email,
+      password: 'password',
+      password_confirmation: 'password'
+    }}
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
 end
