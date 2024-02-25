4c4b91ba95775f540e3c08b49d4d194423d8ad96
KoMaTop10 <y.komachi10@gmail.com>
2024-02-25 12:10:57 +0000

    finished section13

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,87 +1,92 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     before_save { self.email = email.downcase }
 
     validates :name, 
-        presence: true,
-        length: {maximum:50 }
+      presence: true,
+      length: {maximum:50 }
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, 
-        presence: true,
-        length: { maximum:50 },
-        format: { with: VALID_EMAIL_REGEX},
-        uniqueness: true
+      presence: true,
+      length: { maximum:50 },
+      format: { with: VALID_EMAIL_REGEX},
+      uniqueness: true
 
     has_secure_password
     validates :password, presence: true, length: { minimum: 6}
 
     def self.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
-                                                     BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
     end
 
     def self.new_token
-        SecureRandom.urlsafe_base64
+      SecureRandom.urlsafe_base64
     end
 
     def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
-        remember_digest
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+      remember_digest
     end
 
     def session_token
-        remember_digest || remember
+      remember_digest || remember
     end
 
     def authenticated?(attribute, token)
-        digest = send("#{attribute}_digest")
-        return false if digest.nil?
-        BCrypt::Password.new(digest).is_password?(token)
+      digest = send("#{attribute}_digest")
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
     end
 
     def forget 
-        update_attribute(:remember_digest, nil)
+      update_attribute(:remember_digest, nil)
     end
 
     def activate
-        update_columns(
-            activated: true,
-            activated_at: Time.zone.now
-        )
+      update_columns(
+          activated: true,
+          activated_at: Time.zone.now
+      )
     end
 
     def send_activation_email
-        UserMailer.account_activation(self).deliver_now
+      UserMailer.account_activation(self).deliver_now
     end
 
     def create_reset_digest
-        self.reset_token = User.new_token
-        update_columns(
-            reset_digest: User.digest(reset_token),
-            reset_sent_at: Time.zone.now
-        )
+      self.reset_token = User.new_token
+      update_columns(
+          reset_digest: User.digest(reset_token),
+          reset_sent_at: Time.zone.now
+      )
     end
 
     def send_password_reset_email
-        UserMailer.password_reset(self).deliver_now
+      UserMailer.password_reset(self).deliver_now
     end
 
     def password_reset_expired?
-        reset_sent_at < 2.hours.ago
+      reset_sent_at < 2.hours.ago
+    end
+
+    def feed
+      Micropost.where('user_id = ?',id)
     end
     private
     
-        def downcase_email
-            self.email = email.downcase
-        end
+      def downcase_email
+          self.email = email.downcase
+      end
 
-        def create_activation_digest
-            self.activation_token = User.new_token
-            self.activation_digest = User.digest(activation_token)
-        end
+      def create_activation_digest
+          self.activation_token = User.new_token
+          self.activation_digest = User.digest(activation_token)
+      end
 end
